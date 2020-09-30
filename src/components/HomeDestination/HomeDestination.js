9ed5b69aa474d0a9c3834e597527bee624b8dca5
eshansaif <35582062+eshansaif@users.noreply.github.com>
2020-10-01 04:13:57 +0600

    cards background removed

--- src/components/HomeDestination/HomeDestination.js
+++ src/components/HomeDestination/HomeDestination.js
@@ -7,11 +7,11 @@ const HomeDestination = (props) => {
     console.log(props.destination);
     const {destination_id, destination_name, destination_desc, destination_image} = props.destination;
     return (
-        <div className="col-md-4 ">   
+        <div className="col-md-4 " >   
             <Link to={`/booking/${destination_id}`}>     
             <Card className=" text-white home-component">
                 <Card.Img src={destination_image} alt="Card image" />
-                <Card.ImgOverlay className="d-flex align-items-end">
+                <Card.ImgOverlay  className="d-flex align-items-end">
                     <Card.Title> <h3>{destination_name}</h3> </Card.Title>
                 </Card.ImgOverlay>
             </Card>
